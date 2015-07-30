1a9f96284505cf0a2cd8b49be29319fc4b223cc4
Rugal Bernstein <Rugal@users.noreply.github.com>
2015-07-30 16:00:05 +0800

    Update ExceptionAction.java

--- src/main/resources/archetype-resources/src/main/java/rugal/sample/controller/ExceptionAction.java
+++ src/main/resources/archetype-resources/src/main/java/rugal/sample/controller/ExceptionAction.java
@@ -1,11 +1,15 @@
 package rugal.sample.controller;
 
 import ml.rugal.sshcommon.springmvc.controller.BaseExceptionAction;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 /**
  *
  * @author Rugal Bernstein
  */
+@Controller
+@ControllerAdvice
 public class ExceptionAction extends BaseExceptionAction
 {
 
